ca9182f7211f218dddd5612505c5af73bc3113e3
Evgegeni <popovevgeni81@gmail.com>
2022-10-22 10:53:22 +0800

    HW1

---
+++ src/main/java/hw1/Task2.java
@@ -0,0 +1,26 @@
+package hw1;
+
+/*
+Посмотрите на код, и подумайте сколько разных типов исключений вы тут сможете получить?
+public static int sum2d(String[][] arr){ int sum = 0; for (int i = 0; i < arr.length; i++)
+ { for (int j = 0; j < 5; j++) { int val = Integer.parseInt(arr[i][j]); sum += val; } } return sum; }
+
+ NumberFormatException
+ ArrayIndexOutOfBoundsException
+**/
+public class Task2 {
+    public static void main(String[] args) {
+        String[][] arr = new String[][]{{"1","2","3","4","a"}, {"1","2","3","4","5"}, {"1","2","3","4","5"}};
+        System.out.println(sum2d(arr));
+    }
+    public static int sum2d(String[][] arr) {
+        int sum = 0;
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < 5; j++) {
+                int val = Integer.parseInt(arr[i][j]);
+                sum += val;
+            }
+        }
+        return sum;
+    }
+}
